f2abf519c917b7837fb1a2eb7940deb34718209d
Redwan G <redwan@macademia.fr>
2022-07-07 13:57:45 +0200

    ajoute provisoirement la connection à la base de donnée pour la tester

--- gestion_db_performance/src/App.jsx
+++ gestion_db_performance/src/App.jsx
@@ -1,13 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import ConnectionForm from './components/connection/ConnectionForm';
 
 function App() {
+  const mysql = require('mysql');
+
+  const con = mysql.createConnection({
+    host: "localhost:3306",
+    user: "root",
+    password: "",
+    database: 'gestion_dbperformance'
+  });
+
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM users", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
+
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <>
+      <div className="App">
+
+        <div>
+          <ConnectionForm />
+        </div>
+      </div>
+    </>
   );
 }
 
