ef1f5f93aa511dd3e8b340a4f61d07154453e63e
Redwan G <redwan@macademia.fr>
2022-07-08 11:36:12 +0200

    commit

---
+++ gestion_db_performance/frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import ConnectionForm from './components/connection/ConnectionForm';
+
+function App() {
+
+
+  return (
+    <>
+      <div className="App">
+
+        <div>
+          <ConnectionForm />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
